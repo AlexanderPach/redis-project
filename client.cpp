1dca0f5882fa169fb9e1df70fc11516ecbd87b77
AlexanderPach <apachofficial@gmail.com>
2026-09-05 03:04:20 -0700

    [fix] helper functions for msg

--- client.cpp
+++ client.cpp
@@ -7,6 +7,11 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <assert.h>
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
 
 // error handling
 static void die(const char *msg) {
